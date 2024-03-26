da2860de127ef02c1dfcc0658c510c080183f6b9
akbor shanto <akborshanto2002@gmail.com>
2024-03-25 20:50:26 -0700

    navlink

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,48 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import "./index.css";
+import Roots from './layout/Roots.jsx';
+import Error from './pages/Error/Error.jsx';
+import Home from './component/home/Home.jsx';
+import ListedBooks from './pages/ListedBooks/ListedBooks.jsx';
+import PagesRead from './pages/Pages-to-Read/PagesRead.jsx';
 
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element:<Roots></Roots>,
+errorElement:<Error></Error>,
+
+children:[
+{path:'/',
+element:<Home></Home>
+},
+{
+  path:'/listedBook',
+  element:<ListedBooks></ListedBooks>
+},
+{
+  path:'/PagesRead',
+  element:<PagesRead></PagesRead>
+}
+
+
+
+
+]
+
+
+
+  },
+]);
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+<RouterProvider router={router}></RouterProvider>
   </React.StrictMode>,
 )
---
+++ src/pages/Pages-to-Read/PagesRead.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const PagesRead = () => {
+  return (
+    <div>
+      PAGES to read
+    </div>
+  )
+}
+
+export default PagesRead
