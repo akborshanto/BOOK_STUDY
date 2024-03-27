77762e301f3a9f19a0421a4b7a33d67d0721bc22
akbor shanto <akborshanto2002@gmail.com>
2024-03-26 18:58:27 -0700

    chart

--- src/pages/Pages-to-Read/PagesRead.jsx
+++ src/pages/Pages-to-Read/PagesRead.jsx
@@ -1,4 +1,4 @@
-
+import React from 'react';
 import { BarChart, Bar, Cell, XAxis, YAxis, CartesianGrid } from 'recharts';
 
 const colors = ['#0088FE', '#00C49F', '#FFBB28', '#FF8042', 'red', 'pink'];
@@ -55,33 +55,39 @@ const getPath = (x, y, width, height) => {
   Z`;
 };
 
-import React from 'react'
+const TriangleBar = (props) => {
+  const { fill, x, y, width, height } = props;
 
-const PagesRead = () => {
+  return <path d={getPath(x, y, width, height)} stroke="none" fill={fill} />;
+};
+
+export default function App() {
   return (
-    <div>
-    <BarChart
-    width={500}
-    height={300}
-    data={data}
-    margin={{
-      top: 20,
-      right: 30,
-      left: 20,
-      bottom: 5,
-    }}
-  >
-    <CartesianGrid strokeDasharray="3 3" />
-    <XAxis dataKey="name" />
-    <YAxis />
-    <Bar dataKey="uv" fill="#8884d8"  label={{ position: 'top' }}>
-      {data.map((entry, index) => (
-        <Cell key={`cell-${index}`} fill={colors[index % 20]} />
-      ))}
-    </Bar>
-  </BarChart>
-    </div>
-  )
+<div className='  lg:ml-[500px] w-[300px] ml-16  mt-16 pb-16 lg:pb-10'>
+
+<BarChart
+className=''
+  width={300}
+  height={300}
+  data={data}
+  margin={{
+    top: 20,
+    right: 30,
+    left: 20,
+    bottom: 5,
+  }}
+>
+  <CartesianGrid strokeDasharray="3 3" />
+  <XAxis dataKey="name" />
+  <YAxis />
+  <Bar dataKey="uv" fill="#8884d8" shape={<TriangleBar />} label={{ position: 'top' }}>
+    {data.map((entry, index) => (
+      <Cell key={`cell-${index}`} fill={colors[index % 20]} />
+    ))}
+  </Bar>
+</BarChart>
+</div>
+  );
 }
 
-export default PagesRead
+App.demoUrl = 'https://codesandbox.io/s/bar-chart-with-customized-shape-dusth';
